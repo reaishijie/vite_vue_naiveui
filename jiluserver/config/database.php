68fa7457a8adc53c09ea34453d5819472570a137
xiaohui <123865346+reaishijie@users.noreply.github.com>
2025-03-17 15:09:49 +0800

    Delete jiluserver directory
    another repository

--- jiluserver/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => '156.225.18.47',
-    'port' => 3306,
-    'dbname' => 'jilu',
-    'username' => 'jilu',
-    'password' => 'G63MQKrD3tfdhJwN',
-    'charset' => 'utf8mb4'
-];
--- jiluserver/utils/Security.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Security {
-    public static function sanitizeInput($data) {
-        if (is_array($data)) {
-            return array_map([self::class, 'sanitizeInput'], $data);
-        }
-        return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-    }
-} 
